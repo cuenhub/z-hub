5269c1e810c9f696846c135ca71ac09b390cd1f0
cuen <136510780+cuenhub@users.noreply.github.com>
2023-09-25 02:54:13 +0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,6 @@
+
+if game.PlaceId == 13772394625 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/cuenhub/z-hub/main/games/bladeball.lua"))()
+else
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/cuenhub/z-hub/main/games/universal.lua"))()
+end
